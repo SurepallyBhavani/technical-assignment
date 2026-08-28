b26d3095508a1a19f834569537d08fc2d6cfa0ff
SurepallyBhavani <your-github-email@example.com>
2026-08-28 23:37:05 +0530

    sql query 2 added

--- question2/queries.sql
+++ question2/queries.sql
@@ -3,3 +3,14 @@ SELECT COUNT(DISTINCT species) as Types_of_Acacia_Plants
 FROM taxonomy
 WHERE species LIKE 'Acacia%';
 
+//query 2
+SELECT
+    t.species as Wheat_Type,
+    r.length as DNA_Sequence_Length
+FROM taxonomy AS t
+JOIN rfamseq AS r
+    ON t.ncbi_id = r.ncbi_id
+WHERE t.species LIKE '%wheat%'
+ORDER BY r.length DESC
+LIMIT 1;
+
